241c6ec11d665027a28005f229ef4d64877fe06a
Nikolay Osetrov <osetrov.n@gmail.com>
2023-11-17 00:35:39 +0100

    First commit

---
+++ OxaTrade/src/main/java/com/oxaata/trade/OxaTradeApplication.java
@@ -0,0 +1,13 @@
+package com.oxaata.trade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OxaTradeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OxaTradeApplication.class, args);
+	}
+
+}
---
+++ OxaTrade/src/test/java/com/oxaata/trade/OxaTradeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oxaata.trade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OxaTradeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
