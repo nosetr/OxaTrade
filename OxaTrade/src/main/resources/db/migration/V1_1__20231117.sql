afa289c1d022f0b40f73edf32540c92735bb9495
nosetr <osetrov.n@gmail.com>
2023-12-07 02:53:53 +0100

    DB-Migration: oxatrade.addresses, oxatrade.country, oxatrade.organizations was created

--- OxaTrade/src/main/resources/db/migration/V1_1__20231117.sql
+++ OxaTrade/src/main/resources/db/migration/V1_1__20231117.sql
@@ -3,16 +3,16 @@ CREATE SCHEMA IF NOT EXISTS oxatrade;
 -- Create the "users" Table
 CREATE TABLE IF NOT EXISTS oxatrade.users (
     id         SERIAL PRIMARY KEY,
-    email      VARCHAR(64)   NOT NULL UNIQUE,
+    email      VARCHAR(64) NOT NULL UNIQUE,
     password   VARCHAR(2048) NOT NULL,
-    user_role  VARCHAR(32)   NOT NULL DEFAULT "USER",
-    first_name VARCHAR(64)   NOT NULL,
-    last_name  VARCHAR(64)   NOT NULL,
-    title 		 VARCHAR(32),
-    enabled    BOOLEAN       NOT NULL DEFAULT FALSE,
-    created_at TIMESTAMP     NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP     NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-);
+    user_role  VARCHAR(32) NOT NULL DEFAULT "USER",
+    first_name VARCHAR(64) NOT NULL,
+    last_name  VARCHAR(64) DEFAULT NULL,
+    title 		 VARCHAR(32) DEFAULT NULL,
+    enabled    BOOLEAN NOT NULL DEFAULT FALSE,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='Table of users';
 
 -- Unlike MySql, Postgres doesn’t come with inbuilt functionality.
 -- So to solve this problem we will be using Triggers and Procedures.
@@ -33,3 +33,15 @@ CREATE TABLE IF NOT EXISTS oxatrade.users (
 --     ON  users
 --     FOR EACH ROW
 -- EXECUTE PROCEDURE update_updated_at_users();
+
+/*
+ * Create new admin with pass: "12345$aA"
+ */
+INSERT INTO oxatrade.users(
+	email,
+	password,
+	user_role,
+	first_name,
+	last_name,
+	enabled)
+VALUES ("admin@adminov.com", "Ojv3Ym0Rg2atgLuVEhe3Ek9xVWp+zYQSvj51M7L66LA=", "ADMIN", "admin", "adminov", true);
--- OxaTrade/src/main/resources/db/migration/V1_2__20231117.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO oxatrade.users(
-	email,
-	password,
-	user_role,
-	first_name,
-	last_name,
-	enabled)
-VALUES ("admin@adminov.com", "12345678", "ADMIN", "admin", "adminov", true);
